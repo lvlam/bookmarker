c92498290c0c8f010c7f41ee287717ed9dac5f9d
lvlam <liem.lam@gmail.com>
2018-02-11 20:54:42 -0500

    added controllers

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,6 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email}! Your user was registered. Have Fun!`
-    })
-  })
+  app.post('/register',
+    AuthenticationController.register)
 }

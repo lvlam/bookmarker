4ea5bf549b3ebf63e6b65a848127ebd841615c7b
lvlam <liem.lam@gmail.com>
2018-02-12 21:54:52 -0500

    added logout function

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -28,6 +28,13 @@
         @click="navigateTo({name: 'register'})">
         Sign Up
       </v-btn>
+      <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        flat
+        dark
+        @click="logout">
+        Log Out
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -38,6 +45,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
